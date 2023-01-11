1df9b51475d8b26a724f2d13ecae5345a068dc8a
Sammy Towett <towettsammy30@gmail.com>
2023-01-11 14:12:24 +0300

    modify json files and fix js linter errors

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let library = [];
 
-const temp = document.querySelector(".book-template");
-const bookshelf = document.querySelector("#books");
+const temp = document.querySelector('.book-template');
+const bookshelf = document.querySelector('#books');
 let idBook = library.length;
 function Book(title, author) {
   this.id = idBook;
@@ -11,14 +11,14 @@ function Book(title, author) {
 }
 function LibraryLoad() {
   library = JSON.parse(localStorage.library);
-  bookshelf.innerHTML = "";
+  bookshelf.innerHTML = '';
   bookshelf.appendChild(temp);
   for (let i = 0; i < library.length; i += 1) {
     // eslint-disable-next-line no-use-before-define
     DisplayBook(library[i]);
   }
 }
-//save book to local storage
+// save book to local storage
 function SaveBook(title, author) {
   const book = new Book(title, author);
   if (!Array.isArray(library)) {
@@ -29,18 +29,18 @@ function SaveBook(title, author) {
   LibraryLoad();
 }
 
-//Event that happens when submiting the form
-document.getElementById("AddBook").addEventListener("submit", (event) => {
+// Event that happens when submiting the form
+document.getElementById('AddBook').addEventListener('submit', (event) => {
   event.preventDefault();
   const formAddBook = document.forms.AddBook;
   const bookData = new FormData(formAddBook);
-  const bookTitle = bookData.get("title");
-  const bookAuthor = bookData.get("author");
+  const bookTitle = bookData.get('title');
+  const bookAuthor = bookData.get('author');
   formAddBook.reset();
   SaveBook(bookTitle, bookAuthor);
 });
 
-//Function to delete book from library
+// Function to delete book from library
 function DeleteBook(id) {
   library = library.filter((book) => book.id !== id);
   localStorage.library = JSON.stringify(library);
@@ -49,9 +49,9 @@ function DeleteBook(id) {
 // function to display books
 function DisplayBook(book) {
   const clon = temp.content.cloneNode(true);
-  clon.querySelectorAll("p")[0].innerHTML = `book name: ${book.title}`;
-  clon.querySelectorAll("p")[1].innerHTML = `author name: ${book.author}`;
-  clon.querySelector("button").addEventListener("click", () => {
+  clon.querySelectorAll('p')[0].innerHTML = `book name: ${book.title}`;
+  clon.querySelectorAll('p')[1].innerHTML = `author name: ${book.author}`;
+  clon.querySelector('button').addEventListener('click', () => {
     DeleteBook(book.id);
   });
   bookshelf.appendChild(clon);
