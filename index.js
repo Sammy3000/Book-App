0c2bc3c217035da1b418a48188f89fd142a156cc
Sammy Towett <towettsammy30@gmail.com>
2023-01-12 12:39:56 +0300

    modify all files

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-import Book from "./book.js";
-import UI from "./UI.js";
-import Store from "./Store.js";
+import Book from './book.js';
+import UI from './UI.js';
+import Store from './Store.js';
 
 // Event:Display books
 
-document.addEventListener("DOMContentLoaded", UI.displayBooks);
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event:Add a book
 
-document.querySelector("#book-form").addEventListener("submit", (e) => {
+document.querySelector('#book-form').addEventListener('submit', (e) => {
   // prevent default
   e.preventDefault();
   // get form values
-  const tittle = document.querySelector("#tittle").value;
-  const author = document.querySelector("#author").value;
+  const tittle = document.querySelector('#tittle').value;
+  const author = document.querySelector('#author').value;
 
   // Validate
 
@@ -31,37 +31,37 @@ document.querySelector("#book-form").addEventListener("submit", (e) => {
 });
 
 // Event:Remove book from UI
-document.querySelector("#book-list").addEventListener("click", (e) => {
+document.querySelector('#book-list').addEventListener('click', (e) => {
   UI.deleteBook(e.target);
 
   // Remove book from Store
   Store.removeBook(
     e.target.parentElement.previousElementSibling.previousElementSibling
-      .previousElementSibling.textContent
+      .previousElementSibling.textContent,
   );
 });
 
-//Navigation Section
+// Navigation Section
 
-const newlist = document.querySelector(".list-menu");
-const add = document.querySelector(".add-new");
-const contact = document.querySelector(".contact-menu");
-const contactPage = document.querySelector(".contact");
-const formSection = document.querySelector(".form-class");
-const addedBooks = document.querySelector(".booksContainer");
+const newlist = document.querySelector('.list-menu');
+const add = document.querySelector('.add-new');
+const contact = document.querySelector('.contact-menu');
+const contactPage = document.querySelector('.contact');
+const formSection = document.querySelector('.form-class');
+const addedBooks = document.querySelector('.booksContainer');
 
-add.addEventListener("click", () => {
-  formSection.style.display = "flex";
-  addedBooks.style.display = "none";
-  contactPage.style.display = "none";
+add.addEventListener('click', () => {
+  formSection.style.display = 'flex';
+  addedBooks.style.display = 'none';
+  contactPage.style.display = 'none';
 });
-newlist.addEventListener("click", () => {
-  formSection.style.display = "none";
-  contactPage.style.display = "none";
-  addedBooks.style.display = "block";
+newlist.addEventListener('click', () => {
+  formSection.style.display = 'none';
+  contactPage.style.display = 'none';
+  addedBooks.style.display = 'block';
 });
-contact.addEventListener("click", () => {
-  contactPage.style.display = "flex";
-  formSection.style.display = "none";
-  addedBooks.style.display = "none";
+contact.addEventListener('click', () => {
+  contactPage.style.display = 'flex';
+  formSection.style.display = 'none';
+  addedBooks.style.display = 'none';
 });
